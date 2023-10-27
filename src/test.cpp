e124c7f56bca5380f43203a486603126a37bef5e
Minchang Sung <tjdalsckd@gmail.com>
2023-10-27 17:15:56 +0900

    231027

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,15 @@
 
-#include "b3RobotSimulatorClientAPI_NoGUI.h"
-#include "b3RobotSimulatorClientAPI_InternalData.h"
-#include "../Utils/b3Clock.h"
 
+#include "b3RobotSimulatorClientAPI_NoGUI.h"
+#include "../Utils/b3Clock.h"
+#include "LinearMath/btVector3.h"
+#include "btBulletDynamicsCommon.h"
+#include <stdio.h>
+#include <iostream>
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
+#include <thread>
 #define ASSERT_EQ(a, b) assert((a) == (b));
 
 int main(int argc, char* argv[])
@@ -13,27 +17,52 @@ int main(int argc, char* argv[])
 
 	b3RobotSimulatorClientAPI_NoGUI* sim = new b3RobotSimulatorClientAPI_NoGUI();
 	bool isConnected = sim->connect(eCONNECT_DIRECT);
-	sim->resetSimulation();
-	sim->setGravity(btVector3(0, 0, -9.8));
-	sim->setNumSolverIterations(100);
-	b3RobotSimulatorSetPhysicsEngineParameters args;
-	double CONTROL_RATE = 1000.0;
+	if (!isConnected)
+	{
+		printf("Cannot connect\n");
+		return -1;
+	}
+	sim->configureDebugVisualizer(COV_ENABLE_GUI, 0);
+	sim->setTimeOut(10);
+	sim->syncBodies();	
+	double CONTROL_RATE = 240.0;
 	btScalar fixedTimeStep = 1. / CONTROL_RATE;
-	sim->setTimeStep(fixedTimeStep);	
-	sim->getPhysicsEngineParameters(args);
+	sim->setTimeStep(fixedTimeStep);
 
-	int planeUid = sim->loadURDF("../urdf/plane/plane.urdf");
-	printf("planeUid = %d\n", planeUid);	
 	
+	btQuaternion q = sim->getQuaternionFromEuler(btVector3(0.1, 0.2, 0.3));
+	//btVector3 rpy;
+	//rpy = sim->getEulerFromQuaternion(q);
+
+	sim->setGravity(btVector3(0, 0, -9.8));
+	//b3RobotSimulatorSetPhysicsEngineParameters args;
+	
+	//btScalar fixedTimeStep = 1. / CONTROL_RATE;
+	//sim->setTimeStep(fixedTimeStep);	
+	//sim->getPhysicsEngineParameters(args);
+	int robotId = sim->loadURDF("../urdf/satellite/arm.urdf");
+	sim->setRealTimeSimulation(false);
+	
+	
+	printf("robotId = %d\n", robotId);	
+	printf("robotId #joints = %d\n",sim->getNumJoints(robotId));
+	printf("isconnected : %d\n",sim->isConnected());
 	double time = 0.0;
 	double dt = fixedTimeStep;
 
 	static int print_cnt =0;
-	while (sim->isConnected())
+	while (sim->canSubmitCommand())
 	{
+
+    	b3JointSensorState jointStates;
+	    bool isState = sim->getJointState(robotId,1, &jointStates);
+    
+
 		time+=dt;
-		if (++print_cnt>=1000){
+		if (++print_cnt>=60){
 				printf("%.3f\n",time);
+				std::cout<<isState<<","<<jointStates.m_jointPosition<<std::endl;
+
 				print_cnt = 0;
 		}
 		sim->stepSimulation();
