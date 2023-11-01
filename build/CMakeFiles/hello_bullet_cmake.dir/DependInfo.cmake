d65eb1c439ec94492819178ed5aee1038cb90009
SHKim-HYU <tjsghd101@naver.com>
2023-11-01 17:24:48 +0900

    cmake module update

---
+++ build/CMakeFiles/hello_bullet_cmake.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robot/robot_ws/bullet3_NoGUI_cmake_template/src/test.cpp" "/home/robot/robot_ws/bullet3_NoGUI_cmake_template/build/CMakeFiles/hello_bullet_cmake.dir/src/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BT_USE_DOUBLE_PRECISION"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/bullet"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hello_bullet_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_bullet_cmake.dir/src/test.cpp.o"
+  "hello_bullet_cmake"
+  "hello_bullet_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_bullet_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
